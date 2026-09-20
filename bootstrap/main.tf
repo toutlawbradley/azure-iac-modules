bbd12ac731b00a9cedea5272b2d4da852d07b87b
Toutlaw Bradley <toutlawbradley@users.noreply.github.com>
2026-09-19 21:21:49 -0500

    fix: consolidate required_providers and add missing required_version
    - Add required_version to bootstrap/main.tf (previously missed in the last fix pass)
    - Merge the separate random provider declaration into the same required_providers block as azurerm in modules/database and modules/key-vault, rather than two separate blocks

--- bootstrap/main.tf
+++ bootstrap/main.tf
@@ -1,8 +1,10 @@
 terraform {
+  required_version = ">= 1.5.0"
+
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~>4.0"
+      version = "~> 4.0"
     }
   }
 }
