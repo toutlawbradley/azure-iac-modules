689d9b28949b8603b8d331924724386131b3cc1b
Toutlaw Bradley <toutlawbradley@users.noreply.github.com>
2026-09-19 21:58:33 -0500

    fix: removed duplicate account replication type

--- bootstrap/main.tf
+++ bootstrap/main.tf
@@ -23,7 +23,6 @@ resource "azurerm_storage_account" "main" {
   resource_group_name           = azurerm_resource_group.main.name
   location                      = "eastus"
   account_tier                  = "Standard"
-  account_replication_type      = "LRS"
   public_network_access_enabled = false
 
   network_rules {
